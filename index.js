863cf5ec7113881e1b61990c02ece43959bfc970
Md Rakibul Hasan <hello.rakibh@gmail.com>
2024-02-19 21:19:11 +0600

    Function

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+// Create a function which take two number from user and return the sum
 function add(a, b) {
-  return a + b;
+  const sum = a + b;
+  const mul = a * b;
+  const sub = a - b;
+  const div = a / b;
+  return {
+    sum,
+    mul,
+    sub,
+    div,
+  };
 }
-let sum = add(3, 8);
-console.log(sum);
+
+const result = add(3, 8);
+console.log(result);

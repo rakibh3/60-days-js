1756ccd9f45bf88cee07fa8f3ec2cb47b653e4c5
Md Rakibul Hasan <hello.rakibh@gmail.com>
2024-03-24 22:32:31 +0600

    Filter

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const numbers = [1, 2, 3, 4, 5];
 // Using the filter method to filter out even numbers
 const evenNumbers = numbers.filter((num) => num % 2 === 0);
 
-console.log(evenNumbers); // Output: [2, 4]
+console.log(evenNumbers);

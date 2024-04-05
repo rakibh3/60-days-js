156b28beda208dff3d41056e341120bab1bac937
Md Rakibul Hasan <hello.rakibh@gmail.com>
2024-04-05 12:59:50 +0600

    For

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const name = ['John', 'Doe', 'Jane', 'Doe'];
 
-// for loop to iterate over the array and print the elements
+// for loop to iterate over the array and print the elements to the console
 for (let i = 0; i < array.length; i++) {
   console.log(name[i]);
 }

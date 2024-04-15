fbc2945560b21d54f805196c51e6eb31f0255a00
Md Rakibul Hasan <hello.rakibh@gmail.com>
2024-04-15 22:02:20 +0600

    For

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ for (let i = 0; i < name.length; i++) {
   console.log(name[i]);
 }
 const age = [3, 8, 4];
+
+// for loop to iterate over the array
+for (let i = 0; i < age.length; i++) {
+  console.log(age[i]);
+}
